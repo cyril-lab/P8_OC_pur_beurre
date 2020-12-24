22f959be2a82520ab98881087188220a12ea3c07
Cyril Vernhes <c.vernhes3@gmail.com>
2020-12-24 18:08:32 +0100

    launching to production

--- pur_beurre/settings.py
+++ pur_beurre/settings.py
@@ -153,11 +153,11 @@ AUTH_USER_MODEL = 'substitute.user'
 
 if os.environ.get('ENV') == 'production':
 
-    # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    BASE_DIR = BASE_DIR = Path(__file__).resolve().parent
 
     # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/1.9/howto/static-files/
-    # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = BASE_DIR / 'staticfiles',
     # STATIC_URL = '/static/'
 
     # Extra places for collectstatic to find static files.
