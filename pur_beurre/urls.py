803d19b9874b355ec35dbca6b7d429a4a3a75f10
Cyril Vernhes <c.vernhes3@gmail.com>
2020-12-27 11:29:22 +0100

    launching to production

--- pur_beurre/urls.py
+++ pur_beurre/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-from . import views
+from pur_beurre import views
 
 urlpatterns = [
     # path('', include('substitute.urls')),
