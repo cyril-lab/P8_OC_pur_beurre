167553b4cfae4da16b053a4b8eb7b1a4d6f496a6
Cyril Vernhes <c.vernhes3@gmail.com>
2020-12-27 11:15:22 +0100

    launching to production

--- pur_beurre/urls.py
+++ pur_beurre/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('/', include('substitute.urls')),
+    path('index/', include('substitute.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
 ]
