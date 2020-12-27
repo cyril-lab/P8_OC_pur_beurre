d41611f1d83e21563c32944ad8849385bb79cb82
Cyril Vernhes <c.vernhes3@gmail.com>
2020-12-27 13:38:19 +0100

    launching to production

--- pur_beurre/urls.py
+++ pur_beurre/urls.py
@@ -20,7 +20,7 @@ from pur_beurre import views
 
 urlpatterns = [
     # path('index/', views.homepage, name='homepage'),
-    path('', include('substitute.urls')),
+    path('legal/', include('substitute.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
 ]
