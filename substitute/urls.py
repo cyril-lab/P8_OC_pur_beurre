2aa3ef0bd6bc3e98de130983944f691f1696f676
Cyril Vernhes <c.vernhes3@gmail.com>
2020-12-27 13:43:53 +0100

    launching to production

--- substitute/urls.py
+++ substitute/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/', views.homepage, name='homepage'),
-    # path('legal/', views.legal, name='legal'),
+    # path('/', views.homepage, name='homepage'),
+
+    path('index/', views.homepage, name='homepage'),
+
 
     path('search/', views.search, name='search'),
     path('product/<int:pk>/', views.product, name='product'),
